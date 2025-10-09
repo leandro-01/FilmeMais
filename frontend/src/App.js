4e7fb11cef7025189e56afef531c2359932bca70
leandro-01 <leandromateustech@gmail.com>
2025-10-09 12:28:49 -0300

    Estrutura inicial do projeto FilmeMais em React

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>FilmeMais</h1>
+      <p>Bem-vindo ao catálogo de filmes!</p>
+    </div>
+  );
+}
+
+export default App;
